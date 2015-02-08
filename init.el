00ef1d1b200c5c434e048cf4e12b6bcf747e4c7a
Ryo Suzuki <ryooopan@googlemail.com>
2015-02-08 17:45:33 +0900

    add popwin

--- init.el
+++ init.el
@@ -9,6 +9,38 @@
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
+(require 'anything-startup)
+(require 'popwin)
+(setq display-buffer-function 'popwin:display-buffer)
+
+;;(require 'auto-complete)
+;;(require 'auto-complete-config)
+;;(global-auto-complete-mode t)
+ 
+;; Multiple Cursor
+(require 'multiple-cursors)
+(require 'smartrep)
+(declare-function smartrep-define-key "smartrep")
+(global-set-key (kbd "C-M-c") 'mc/edit-lines)
+(global-set-key (kbd "C-M-r") 'mc/mark-all-in-region)
+(global-unset-key "\C-t")
+(smartrep-define-key
+ global-map "C-t"
+ '(("C-t"      . 'mc/mark-next-like-this)
+   ("n"        . 'mc/mark-next-like-this)
+   ("p"        . 'mc/mark-previous-like-this)
+   ("m"        . 'mc/mark-more-like-this-extended)
+   ("u"        . 'mc/unmark-next-like-this)
+   ("U"        . 'mc/unmark-previous-like-this)
+   ("s"        . 'mc/skip-to-next-like-this)
+   ("S"        . 'mc/skip-to-previous-like-this)
+   ("*"        . 'mc/mark-all-like-this)
+   ("d"        . 'mc/mark-all-like-this-dwim)
+   ("i"        . 'mc/insert-numbers)
+   ("o"        . 'mc/sort-regions)
+   ("O"        . 'mc/reverse-regions)))
+
+
 ;; Line Number
 (column-number-mode t)
 (line-number-mode t)
